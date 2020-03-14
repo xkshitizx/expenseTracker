2d00ddae22d759a24f2e17115732f4d31f45105f
xkshitizx <kshitizlama03@gmail.com>
2020-03-14 23:20:04 +0545

    added option to take user input

--- exp.py
+++ exp.py
@@ -1,1 +1,23 @@
-print("Choose a category:\n 1. Food \n2. Clothing \n 3. Education\n 4. Travel" )
+import datetime
+i=0
+
+content={
+    '{i}':{
+            "Category":'{cat}',
+            "Money":'{money}',
+            "Date":'{d}'
+        }
+}
+
+cat=input("Choose a category:\n 1. Food \n 2. Clothing \n 3. Education\n 4. Travel")
+d=datetime.datetime.today()
+money=input("how much did you spend??")
+
+content.update({
+    i:{
+            "Category":cat,
+            "Money":money,
+            "Date":d
+        }
+})
+print(content)
